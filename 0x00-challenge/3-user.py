d56b69abf5d38aa85ebd78fc717e41efc8aacf9d
Adane Moges <adanemoges6@gmail.com>
2023-08-26 11:13:13 +0300

    task 3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -6,7 +6,7 @@ import hashlib
 import uuid
 
 
-class User:
+class User():
     """
     User class:
     - id: public string unique (uuid)
@@ -54,10 +54,10 @@ class User:
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print("Test User")
 
     user_1 = User()
@@ -84,17 +84,13 @@ if __name__ == "__main__":
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if user_1.is_valid_password(u_pwd):
-        print(
-            "is_valid_password should return True if it's the right \
-password"
-        )
+    if not user_1.is_valid_password(u_pwd):
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print(
-            "is_valid_password should return False if it's not the right \
-password"
-        )
+        print("is_valid_password should return False if it's not the right \
+password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -103,7 +99,5 @@ password"
         print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print(
-            "is_valid_password should return False if no password set \
-before"
-        )
+        print("is_valid_password should return False if no password set \
+before")
