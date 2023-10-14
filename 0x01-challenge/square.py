4cdf59bd902b1d8ea618cb0617df8eca861907b8
Adane <adanemoges6@gmail.com>
2023-10-14 18:36:59 +0300

    fix squre

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
+"""square module"""
 
-class square():
-    
+
+class square:
+    """thus is squre class"""
+
     width = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """init"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square"""
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 4)
+        """Perimeter of the square"""
+        return self.width * 4
 
     def __str__(self):
+        """str"""
         return "{}/{}".format(self.width, self.width)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12)
     print(s)
     print(s.area_of_my_square())
