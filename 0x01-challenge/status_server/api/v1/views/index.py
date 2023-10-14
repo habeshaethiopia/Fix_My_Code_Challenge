b491c5c5daf9ab9cf35b33318e18d7e31ad09b3b
Adane <adanemoges6@gmail.com>
2023-10-14 18:18:30 +0300

    f2

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Index view
 """
-from flask import jsonify, make_response
+from flask import jsonify
 
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the web server
     """
-    return make_response(jsonify({"status": "OK"}), 200)
+    return jsonify({"status": "OK"})
